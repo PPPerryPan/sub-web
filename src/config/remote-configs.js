21753287b7ed9fb8fe4fddd503185556b4334de0
PPPerryPan <perrypan0123@outlook.com>
2026-04-09 22:56:00 +0800

    add acl4ssr remote configs

--- src/config/remote-configs.js
+++ src/config/remote-configs.js
@@ -31,5 +31,43 @@ export const REMOTE_CONFIGS = [
       { label: "NeteaseUnblock(仅规则，No-Urltest)", value: "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/special/netease.ini" },
       { label: "Basic(仅GEOIP CN + Final)", value: "https://cdn.jsdelivr.net/gh/SleepyHeeead/subconverter-config@master/remote-config/special/basic.ini" }
     ]
+  },
+  {
+    label: "ACL4SSR",
+    options: [
+      { label: "ACL4SSR.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR.ini" },
+      { label: "ACL4SSR_AdblockPlus.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_AdblockPlus.ini" },
+      { label: "ACL4SSR_BackCN.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_BackCN.ini" },
+      { label: "ACL4SSR_Mini.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Mini.ini" },
+      { label: "ACL4SSR_Mini_Fallback.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Mini_Fallback.ini" },
+      { label: "ACL4SSR_Mini_MultiMode.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Mini_MultiMode.ini" },
+      { label: "ACL4SSR_Mini_NoAuto.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Mini_NoAuto.ini" },
+      { label: "ACL4SSR_NoApple.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_NoApple.ini" },
+      { label: "ACL4SSR_NoAuto.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_NoAuto.ini" },
+      { label: "ACL4SSR_NoAuto_NoApple.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_NoAuto_NoApple.ini" },
+      { label: "ACL4SSR_NoAuto_NoApple_NoMicrosoft.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_NoAuto_NoApple_NoMicrosoft.ini" },
+      { label: "ACL4SSR_NoMicrosoft.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_NoMicrosoft.ini" },
+      { label: "ACL4SSR_Online.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini" },
+      { label: "ACL4SSR_Online_AdblockPlus.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_AdblockPlus.ini" },
+      { label: "ACL4SSR_Online_Full.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full.ini" },
+      { label: "ACL4SSR_Online_Full_AdblockPlus.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_AdblockPlus.ini" },
+      { label: "ACL4SSR_Online_Full_Google.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_Google.ini" },
+      { label: "ACL4SSR_Online_Full_MultiMode.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_MultiMode.ini" },
+      { label: "ACL4SSR_Online_Full_Netflix.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_Netflix.ini" },
+      { label: "ACL4SSR_Online_Full_NoAuto.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_NoAuto.ini" },
+      { label: "ACL4SSR_Online_Mini.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini.ini" },
+      { label: "ACL4SSR_Online_Mini_AdblockPlus.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_AdblockPlus.ini" },
+      { label: "ACL4SSR_Online_Mini_Ai.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_Ai.ini" },
+      { label: "ACL4SSR_Online_Mini_Fallback.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_Fallback.ini" },
+      { label: "ACL4SSR_Online_Mini_MultiCountry.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiCountry.ini" },
+      { label: "ACL4SSR_Online_Mini_MultiMode.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini" },
+      { label: "ACL4SSR_Online_Mini_NoAuto.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_NoAuto.ini" },
+      { label: "ACL4SSR_Online_MultiCountry.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_MultiCountry.ini" },
+      { label: "ACL4SSR_Online_NoAuto.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_NoAuto.ini" },
+      { label: "ACL4SSR_Online_NoReject.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_NoReject.ini" },
+      { label: "ACL4SSR_WithChinaIp.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_WithChinaIp.ini" },
+      { label: "ACL4SSR_WithChinaIp_WithGFW.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_WithChinaIp_WithGFW.ini" },
+      { label: "ACL4SSR_WithGFW.ini", value: "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_WithGFW.ini" }
+    ]
   }
 ];
